bb1ceda7a70067cd26889f54653e37455aa48593
Masahiro Hosono <you@example.com>
2021-01-23 17:34:23 +0900

    modify

--- src/main/java/org/example/controller/posttodo/resource/PostTodoResponse.java
+++ src/main/java/org/example/controller/posttodo/resource/PostTodoResponse.java
@@ -1,15 +1,14 @@
-package org.example.controller.resource.postsample;
+package org.example.controller.posttodo.resource;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Value;
 
-@ApiModel(value = "PostSampleResponse", description = "Sample model for the documentation")
 @Value
-public class PostSampleResponse {
+@ApiModel(value = "PostTodoResponse", description = "Sample model for the documentation")
+public class PostTodoResponse {
     @ApiModelProperty(required = true, value = "結果", example = "true", position = 1)
     @JsonProperty("result")
     private final Boolean result;
-
 }
